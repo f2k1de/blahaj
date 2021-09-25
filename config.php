09d20d77f771c0fb1f21c2e723ff2880d769b2bd
x44203 <user65535@protonmail.com>
2021-09-25 12:45:02 +0200

    fix config default

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $kleinerhai = $stock_url . "50455234";
 $grosserhai = $stock_url . "30373588";
 $api_key = "b6c117e5-ae61-4ef5-b4cc-e0b1e37f0631"; // This seems to be constant
 
-$mysql_host = localhost;
+$mysql_host = "localhost";
 $mysql_username = "grafana";
 $mysql_password = "PASSWORD";
 $mysql_database = "grafana_blahaj";
